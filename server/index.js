6b60e5576477b921c9d37a96e55b37f47ca83e7c
zlx362211854 <362211854@qq.com>
2019-12-18 15:26:31 +0800

    new book 💐

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const createAll = require('./createAll')
 const moment = require('moment')
 const  scheduleCronstyle = () => {
   //每天1点1分0秒时更新
-  schedule.scheduleJob('0 01 15 * * *', () => {
+  schedule.scheduleJob('0 26 15 * * *', () => {
       console.log(moment(new Date()).format('YYYY-MM-DD HH:mm:ss') + '开始更新book...')
       createAll()
     }); 
